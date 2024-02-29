28a9593b870e40540ef53a57285c4e3486f86e8c
Oséias Costa <oseiasc2@gmail.com>
2024-02-29 13:19:18 -0300

    feat: add .gitignore

--- init-scripts/ubuntu.sh
+++ init-scripts/ubuntu.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
+function logStep() {
+  echo -e "\n\e[1;32m$1\e[0m"
+}
+
+function saveOld() {
+  echo -e "cp $1 $1-old 2> /dev/null || true"
+}
+
+logStep "create symbolic links"
+
 ln -s ~/dotfiles/.zsh ~/.zsh
 ln -s ~/dotfiles/.zsh/.zshrc ~/.zshrc
-ls -s ~/dotfiles/.config ~/.config
+ln -s ~/dotfiles/.config ~/.config
+ln -s ~/dotfiles/.config/alacritty ~/.config/alacritty
+ln -s ~/dotfiles/.config/spaceship ~/.config/spaceship
